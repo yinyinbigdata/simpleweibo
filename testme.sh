e554a5fc50a5c667ae32d0ea823d062ac6d0ec1e
yin.yin <yin.yin@cs2c.com.cn>
2014-03-21 05:33:02 +0800

    change client path in testme.sh

--- testme.sh
+++ testme.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
-./client/client uc dga
-./client/client uc bryant
-./client/client uc rstarzl
-./client/client uc imoraru
+client uc dga
+client uc bryant
+client uc rstarzl
+client uc imoraru
 
-./client/client sl dga
-./client/client sa dga bryant
-./client/client sl dga
-./client/client sa dga imoraru
-./client/client sl dga
+client sl dga
+client sa dga bryant
+client sl dga
+client sa dga imoraru
+client sl dga
 
-./client/client tl dga
-./client/client tp dga "First post"
-./client/client tl dga
-./client/client tp dga "Second post"
-./client/client tl dga
+client tl dga
+client tp dga "First post"
+client tl dga
+client tp dga "Second post"
+client tl dga
 
-./client/client sa bryant imoraru
-./client/client sa bryant dga
-./client/client tp imoraru "Iulian's first post"
-./client/client tp imoraru "Iulian's second post"
-./client/client ts bryant
+client sa bryant imoraru
+client sa bryant dga
+client tp imoraru "Iulian's first post"
+client tp imoraru "Iulian's second post"
+client ts bryant
